53783e87dc224dbe45ef7ed3ae4c7b0803ec1779
ma0tsu0da <kou81@m_pc.localdomain>
2024-09-13 02:15:40 +0900

    container

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ st.set_page_config(
     }
 )
 
+map_center = [35.686086, 139.760256]  # 千代田区
+
 
 def add_choropleth(my_map, df_map: pd.DataFrame, col_name: str, fill_color: str, bins: list[float]):
     folium.Choropleth(
@@ -64,125 +66,130 @@ df_map = gpd.GeoDataFrame(df_map, geometry='geometry')
 df_map = df_map.set_crs(epsg=4612, inplace=True)
 
 
-placeholder_1 = st.empty()
-map_col_1, menu_col_1 = placeholder_1.columns([5, 2])
-placeholder_2 = st.empty()
-map_col_2, menu_col_2 = placeholder_2.columns([5, 2])
+# placeholder_1 = st.empty()
+# map_col_1, menu_col_1 = placeholder_1.columns([5, 2])
+# placeholder_2 = st.empty()
+# map_col_2, menu_col_2 = placeholder_2.columns([5, 2])
 
-with menu_col_1:
-    # カスタムCSSを追加してラベルの折り返しを防ぐ
-    st.markdown("""
-    <style>
-    .stMarkdown h2 {
-        white-space: nowrap;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+with st.container():
+    map_col_1, menu_col_1 = st.columns([5, 2])
 
-    st.header("マップ1 分割設定")
-    division = st.number_input(
-        label='分割数',
-        value=9,
-        min_value=4,
-        max_value=100,
-        key="division_1"
-    )
-    st.header("マップ1 閾値設定")
-    thresholds_1 = []
-    for i in range(st.session_state["division_1"]):
-        if i == 0:
-            threshold_1 = int(np.floor(df_map["高校生数"].min()))
-        elif i == st.session_state["division_1"] - 1:
-            threshold_1 = int(np.ceil(df_map["高校生数"].max()))
-        else:
-            threshold_1 = st.number_input(
-                f"閾値_{i}", min_value=0, max_value=1000, value=i * 25)
-        thresholds_1.append(threshold_1)
+    with menu_col_1:
+        # カスタムCSSを追加してラベルの折り返しを防ぐ
+        st.markdown("""
+        <style>
+        .stMarkdown h2 {
+            white-space: nowrap;
+        }
+        </style>
+        """, unsafe_allow_html=True)
 
-    # Thresholdリストを表示
-    st.write("最小値:", df_map["高校生数"].min())
-    st.write("最大値:", df_map["高校生数"].max())
+        st.header("マップ1 分割設定")
+        division = st.number_input(
+            label='分割数',
+            value=9,
+            min_value=4,
+            max_value=100,
+            key="division_1"
+        )
+        st.header("マップ1 閾値設定")
+        thresholds_1 = []
+        for i in range(st.session_state["division_1"]):
+            if i == 0:
+                threshold_1 = int(np.floor(df_map["高校生数"].min()))
+            elif i == st.session_state["division_1"] - 1:
+                threshold_1 = int(np.ceil(df_map["高校生数"].max()))
+            else:
+                threshold_1 = st.number_input(
+                    f"閾値_{i}", min_value=0, max_value=1000, value=i * 25)
+            thresholds_1.append(threshold_1)
 
-with menu_col_2:
-    # カスタムCSSを追加してラベルの折り返しを防ぐ
-    st.markdown("""
-    <style>
-    .stMarkdown h2 {
-        white-space: nowrap;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+        # Thresholdリストを表示
+        st.write("最小値:", df_map["高校生数"].min())
+        st.write("最大値:", df_map["高校生数"].max())
 
-    st.header("マップ2 分割設定")
-    division = st.number_input(
-        label='分割数',
-        value=9,
-        min_value=4,
-        max_value=100,
-        key="division_2"
-    )
-    st.header("マップ2 閾値設定")
-    thresholds_2 = []
-    for i in range(st.session_state["division_2"]):
-        if i == 0:
-            threshold_2 = int(np.floor(df_map["平均年齢"].min()))
-        elif i == st.session_state["division_2"] - 1:
-            threshold_2 = int(np.ceil(df_map["平均年齢"].max()))
-        else:
-            threshold_2 = st.number_input(
-                f"閾値_{i}", min_value=0, max_value=100, value=15 + i * 7)
-        thresholds_2.append(threshold_2)
+    with map_col_1:
+        # def create_map(my_map: folium.Map):
+        #    add_choropleth(my_map, df_map, num_student, "BuGn", thresholds)
+        #    add_choropleth(my_map, df_map, ave_age, "RdPu", [0, 30, 35, 40, 45, 50, 55, 70])
+        #    folium.GeoJson(
+        #        df_map,
+        #        style_function=lambda x: {'fillColor': '#ffffff00', 'color': '#ffffff00'},
+        #        tooltip=GeoJsonTooltip(
+        #            fields=["NAME", "高校生数", "平均年齢"],  # ツールチップに表示するフィールド名
+        #            aliases=["地域名: ", "高校生数: ", "平均年齢: ",],  # フィールド名の別名
+        #            localize=True,  # データをローカライズするか（数値フォーマットなど）
+        #            sticky=False,  # マウスが動いてもツールチップを表示し続けるか
+        #            labels=True,  # ラベルを表示するか
+        #            style=("background-color: white; color: black; font-weight: bold; padding: 5px; z-index: 1000")
+        #        ),
+        #        name="tooltip",
+        #        show=True
+        #    ).add_to(my_map)
+        #    folium.LayerControl().add_to(my_map)
 
-    # Thresholdリストを表示
-    st.write("最小値:", df_map["平均年齢"].min())
-    st.write("最大値:", df_map["平均年齢"].max())
+        # ボタンの状態を session_state で管理
+        if 'show_map1' not in st.session_state:
+            st.session_state['show_map1'] = False
 
-map_center = [35.686086, 139.760256]  # 千代田区
+        # ボタン1が押された場合
+        if st.button("マップ1生成", key="button_1"):
+            st.session_state['show_map1'] = not st.session_state['show_map1']
 
-with map_col_1:
-    # def create_map(my_map: folium.Map):
-    #    add_choropleth(my_map, df_map, num_student, "BuGn", thresholds)
-    #    add_choropleth(my_map, df_map, ave_age, "RdPu", [0, 30, 35, 40, 45, 50, 55, 70])
-    #    folium.GeoJson(
-    #        df_map,
-    #        style_function=lambda x: {'fillColor': '#ffffff00', 'color': '#ffffff00'},
-    #        tooltip=GeoJsonTooltip(
-    #            fields=["NAME", "高校生数", "平均年齢"],  # ツールチップに表示するフィールド名
-    #            aliases=["地域名: ", "高校生数: ", "平均年齢: ",],  # フィールド名の別名
-    #            localize=True,  # データをローカライズするか（数値フォーマットなど）
-    #            sticky=False,  # マウスが動いてもツールチップを表示し続けるか
-    #            labels=True,  # ラベルを表示するか
-    #            style=("background-color: white; color: black; font-weight: bold; padding: 5px; z-index: 1000")
-    #        ),
-    #        name="tooltip",
-    #        show=True
-    #    ).add_to(my_map)
-    #    folium.LayerControl().add_to(my_map)
+        if st.session_state['show_map1'] & st.session_state["division_1"]:
+            st.write("マップ1")
+            my_map_1 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
+            create_map(my_map_1, df_map, "高校生数", "BuGn", thresholds_1)
+            st_folium(my_map_1, use_container_width=True, height=350, returned_objects=[])
 
-    # ボタンの状態を session_state で管理
-    if 'show_map1' not in st.session_state:
-        st.session_state['show_map1'] = False
 
-    # ボタン1が押された場合
-    if st.button("マップ1生成", key="button_1"):
-        st.session_state['show_map1'] = not st.session_state['show_map1']
+with st.container():
+    map_col_2, menu_col_2 = st.columns([5, 2])
 
-    if st.session_state['show_map1'] & st.session_state["division_1"]:
-        st.write("マップ1")
-        my_map_1 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
-        create_map(my_map_1, df_map, "高校生数", "BuGn", thresholds_1)
-        st_folium(my_map_1, use_container_width=True, height=350, returned_objects=[])
+    with menu_col_2:
+        # カスタムCSSを追加してラベルの折り返しを防ぐ
+        st.markdown("""
+        <style>
+        .stMarkdown h2 {
+            white-space: nowrap;
+        }
+        </style>
+        """, unsafe_allow_html=True)
 
-with map_col_2:
-    if 'show_map2' not in st.session_state:
-        st.session_state['show_map2'] = False
+        st.header("マップ2 分割設定")
+        division = st.number_input(
+            label='分割数',
+            value=9,
+            min_value=4,
+            max_value=100,
+            key="division_2"
+        )
+        st.header("マップ2 閾値設定")
+        thresholds_2 = []
+        for i in range(st.session_state["division_2"]):
+            if i == 0:
+                threshold_2 = int(np.floor(df_map["平均年齢"].min()))
+            elif i == st.session_state["division_2"] - 1:
+                threshold_2 = int(np.ceil(df_map["平均年齢"].max()))
+            else:
+                threshold_2 = st.number_input(
+                    f"閾値_{i}", min_value=0, max_value=100, value=15 + i * 7)
+            thresholds_2.append(threshold_2)
 
-    # ボタン2が押された場合
-    if st.button("マップ2生成", key="button_2"):
-        st.session_state['show_map2'] = not st.session_state['show_map2']
+        # Thresholdリストを表示
+        st.write("最小値:", df_map["平均年齢"].min())
+        st.write("最大値:", df_map["平均年齢"].max())
 
-    if st.session_state['show_map2'] & st.session_state["division_2"]:
-        st.write("マップ2")
-        my_map_2 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
-        create_map(my_map_2, df_map, "平均年齢", "RdPu", thresholds_2)
-        st_folium(my_map_2, use_container_width=True, height=350, returned_objects=[])
+    with map_col_2:
+        if 'show_map2' not in st.session_state:
+            st.session_state['show_map2'] = False
+
+        # ボタン2が押された場合
+        if st.button("マップ2生成", key="button_2"):
+            st.session_state['show_map2'] = not st.session_state['show_map2']
+
+        if st.session_state['show_map2'] & st.session_state["division_2"]:
+            st.write("マップ2")
+            my_map_2 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
+            create_map(my_map_2, df_map, "平均年齢", "RdPu", thresholds_2)
+            st_folium(my_map_2, use_container_width=True, height=350, returned_objects=[])
