b5e6c7aaa9a2a2392bf1682fd33441014b1fd943
ma0tsu0da <kou81@m_pc.localdomain>
2024-09-13 01:26:26 +0900

    height

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ with map_col_1:
         st.write("マップ1")
         my_map_1 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
         create_map(my_map_1, df_map, "高校生数", "BuGn", thresholds_1)
-        st_folium(my_map_1, use_container_width=True, width=1200, height=720, returned_objects=[])
+        st_folium(my_map_1, use_container_width=True, width=1200, height=300, returned_objects=[])
 
 with map_col_2:
     if 'show_map2' not in st.session_state:
@@ -172,4 +172,4 @@ with map_col_2:
         st.write("マップ2")
         my_map_2 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
         create_map(my_map_2, df_map, "平均年齢", "RdPu", thresholds_2)
-        st_folium(my_map_2, use_container_width=True, width=1200, height=720, returned_objects=[])
+        st_folium(my_map_2, use_container_width=True, width=1200, height=300, returned_objects=[])
