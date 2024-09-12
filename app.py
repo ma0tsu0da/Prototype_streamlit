a3439a63195ef6c91bcb82e0a42f461379c0c253
ma0tsu0da <kou81@m_pc.localdomain>
2024-09-13 03:37:33 +0900

    fix

--- app.py
+++ app.py
@@ -254,10 +254,6 @@ with st.container():
         if 'show_map51' not in st.session_state:
             st.session_state['show_map51'] = False
 
-        # ボタン2が押された場合
-        if st.button("マップ5生成", key="button_51"):
-            st.session_state['show_map51'] = not st.session_state['show_map51']
-
         if st.session_state['show_map51']:
             st.header("高校生数マッピング")
             my_map_1 = folium.Map(location=map_center, tiles='openstreetmap', zoom_start=13)
